afbc646c7f61338dea85f9e303704101f4ecbc4f
t <tony@hughessolutions.com>
2022-04-05 14:47:16 -0700

    Added DARE and disk examples

---
+++ PE_v2_disks_uuid.py
@@ -0,0 +1,57 @@
+"""
+02/26/2022 TH: Get disk info from PE
+"""
+import requests
+import urllib3
+import json
+import os
+import sys
+from dotenv import load_dotenv
+from pathlib import Path
+from requests.auth import HTTPBasicAuth
+
+
+def main():
+    # load the script configuration
+    env_path = Path(".") / ".env"
+    load_dotenv(dotenv_path=env_path)
+    PC_IP = os.getenv("PC_IP")
+    PC_PORT = os.getenv("PC_PORT")
+    PC_USERNAME = os.getenv("PC_USERNAME")
+    PC_PASSWORD = os.getenv("PC_PASSWORD")
+    CLUSTER_IP = os.getenv("CLUSTER_IP")
+    CLUSTER_PORT = os.getenv("CLUSTER_PORT")
+    CLUSTER_USERNAME = os.getenv("CLUSTER_USERNAME")
+    CLUSTER_PASSWORD = os.getenv("CLUSTER_PASSWORD")
+
+    # don't worry about invalid certs
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    # setup the API request
+    # UUID of first disk listed from disks api, contents looks the same except it only contains the 1 disk
+    endpoint = f"https://{CLUSTER_IP}:{CLUSTER_PORT}/PrismGateway/services/rest/v2.0/disks/1348b1dd-b196-4a42-9af2-f5ae39d0dd9a/"
+    request_headers = {"Content-Type": "application/json", "charset": "utf-8"}
+    request_body = {}
+
+    # Submit the requests and get the output
+    try:
+        results = requests.get(
+            endpoint,
+            data=json.dumps(request_body),
+            headers=request_headers,
+            verify=False,
+            auth=HTTPBasicAuth(CLUSTER_USERNAME, CLUSTER_PASSWORD),
+        )
+
+        # check the results of the request
+        if results.status_code == 200 or results.status_code == 201:
+            print(json.dumps(results.json(), indent=4, sort_keys=True))
+
+    except Exception as error:
+        print(f"ERROR: {error}")
+        print(f"Exception: {error.__class__.__name__}")
+        sys.exit()
+
+
+if __name__ == "__main__":
+    main()
